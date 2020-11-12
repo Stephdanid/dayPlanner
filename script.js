1c981b7d6ecd4e1a27e28fd9f806a526201d517b
stephdanid <stephaniedanidominguez@gmail.com>
2020-11-11 23:03:33 -0600

    added day.js cdn and managed to display the current day in the correct html tag

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // displayed current day at the top of calendar
-let showCalendar = document.querySelector("#currentDay").textContent = "luxon display";
-
-
-// console.log(DateTime.local().toLocaleString();)
-// DATETIME_MED: Object
-// DateTime.local().toLocaleString();
-// toLocaleString 
-if (conditonal based on time in library) {
-    code to run change to past, present.future in styling
-}
-setattrbute to past
-// create grid of time blocks that would contain text areas
-// color code according to time
-//  on save we would save to local storage
-// on page load fill in time blocks on local storage
+let showCalendar = document.querySelector("#currentDay");
+dayjs().format()
+console.log(dayjs());
+showCalendar.textContent = dayjs()
+    // if (conditonal based on time in library) {
+    //     code to run change to past, present.future in styling
+    // }
+    // setattrbute to past
+    // create grid of time blocks that would contain text areas
+    // color code according to time
+    //  on save we would save to local storage
+    // on page load fill in time blocks on local storage
