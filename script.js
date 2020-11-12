fde79b65d8ff03bf345d16501426e3db00024edc
stephdanid <stephaniedanidominguez@gmail.com>
2020-11-11 22:14:28 -0600

    added other hours needed as static html and wrote my thought process in javascript

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // displayed current day at the top of calendar
-let showCalendar = document.querySelector("#currentDay").textContent = "DateTime.local().toLocaleString()";
+let showCalendar = document.querySelector("#currentDay").textContent = "luxon display";
 
 
-
-
-
-
+// console.log(DateTime.local().toLocaleString();)
+// DATETIME_MED: Object
+// DateTime.local().toLocaleString();
+// toLocaleString 
+if (conditonal based on time in library) {
+    code to run change to past, present.future in styling
+}
+setattrbute to past
 // create grid of time blocks that would contain text areas
 // color code according to time
 //  on save we would save to local storage
